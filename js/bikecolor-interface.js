8285f7d776c6101300d0bdffb2ce5564f9e73ae4
Cassandra Culkins and Mao Thygesen <cassy.culkins@gmail.com>
2016-03-16 16:42:55 -0700

    add styling

--- js/bikecolor-interface.js
+++ js/bikecolor-interface.js
@@ -4,12 +4,16 @@ var apiKey = require('./../.env').apiKey;
 
 $(document).ready(function() {
   $('#submitButton').click(function() {
-    var color = $('#colorInput').val();
+    var colorArray = [];
+    $(':checkbox:checked').each(function(color){
+      colorArray[color] = $(this).val();
+    });
+    // var color = $('input[name="color"]:checked').val();
     var location = $('#locationInput').val();
     $('#colorInput').val("");
     $('#locationInput').val("");
-    $.get('https://bikeindex.org/api/v2/bikes_search/count?page=1&colors=' + color + '&proximity=' + location + '&proximity_square=100&access_token=' + apiKey).then(function(response) {
-      $('.showResult').text('Here are the number of stolen bikes so far in ' + location + ' that are the color ' + color + ': ' + response.stolen)
-    })
+    $.get('https://bikeindex.org/api/v2/bikes_search/count?page=1&colors=' + colorArray + '&proximity=' + location + '&proximity_square=100&access_token=' + apiKey).then(function(response) {
+      $('.showResult').text('Here are the number of stolen bikes so far in ' + location + ' that are the color ' + colorArray + ': ' + response.stolen);
+    });
   });
 });
